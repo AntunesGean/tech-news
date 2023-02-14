c50d08c6b09cdedd7750439d9bee8c6977b8b11e
AntunesGean <gean.antunes@gmail.com>
2023-02-14 20:29:26 -0300

    req 07 - ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+
+    search_news_list = []
+
+    for news_one in search_news({"title": {"$regex": title, "$options": "i"}}):
+        search_news_list.append((news_one["title"], news_one["url"]))
+
+    return search_news_list
 
 
 # Requisito 8
