6afa17d92684feaabe34fd15cdfb2bcf9794e26c
AntunesGean <gean.antunes@gmail.com>
2023-02-14 19:09:07 -0300

    req 04 - ok

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -41,7 +41,25 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+
+    selector = Selector(html_content)
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css(".meta-author .author a::text").get()
+    read_time = selector.css(".meta-reading-time::text").re_first(r"\d+")
+    summary = selector.xpath("string(//p)").get().strip()
+    category = selector.css(".category-style .label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(read_time),
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
